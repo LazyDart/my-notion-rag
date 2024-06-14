a2f70df0b0fc9337ca3abca547f4df284f21a78e
Lazy Dart <estp.pl@gmail.com>
2024-06-14 08:13:21 +0200

    Improved notion db reading functionality

--- src/scrape_notion_database.py
+++ src/scrape_notion_database.py
@@ -13,12 +13,35 @@ header = headers = {
     "Notion-Version": "2022-06-28",
 }
 
-payload = {"page_size": 100}
-
 notes_id = databases[1]["id"]
 
-response = requests.post(f"https://api.notion.com/v1/databases/{notes_id}/query", headers=header, json=payload)
+def get_db_pages(database_id, header, num_pages=None):
+    
+    assert num_pages is None or 0 < num_pages < 101, "num_pages must be None or a positive integer (0, 100]"
 
-data = response.json()
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    
+    get_all = num_pages is None
+    page_size = 100 if get_all else num_pages
 
-print(data)
+    payload = {"page_size": page_size}
+
+    response = requests.post(url, headers=header, json=payload)
+    data = response.json()
+
+    results = data["results"]
+
+    while data["has_more"] and get_all:
+        payload["start_cursor"] = data["next_cursor"]
+
+        response = requests.post(url, headers=header, json=payload)
+        data = response.json()
+        
+        results.extend(data["results"])
+
+    return results
+
+notes = get_db_pages(notes_id, header)
+
+with open('db.json', 'w', encoding='utf8') as f:
+    json.dump(notes, f, ensure_ascii=False, indent=4)
